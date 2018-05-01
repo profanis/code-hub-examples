af9102d60b7d886b28ef5a313fe79bc40bb1727c
Fanis Prodromou <fanis@projectbeagle.com>
2018-05-01 20:47:09 +0300

    reactive forms with verbose validation in markup

--- src/app/modules/reactive-form/reactive-form.module.ts
+++ src/app/modules/reactive-form/reactive-form.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormComponent } from "./reactive-form/reactive-form.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     ReactiveFormsModule
   ],
   declarations: [ReactiveFormComponent],
