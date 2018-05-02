7e2bea38d6c7f61e2564b5a2aee65734f3e06223
profanis <fanis@projectbeagle.com>
2018-05-02 18:53:49 +0300

    reactive forms validations

--- src/app/modules/reactive-form/reactive-form/reactive-form.component.ts
+++ src/app/modules/reactive-form/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators, AbstractControl } from "@angular/forms";
 import { Customer } from "../../basic-form/basic-form/cusomer.model";
 
 @Component({
@@ -14,10 +14,20 @@ export class ReactiveFormComponent implements OnInit {
 
   constructor() { }
 
+  firstNameFormControl = new FormControl("", [Validators.required, Validators.minLength(3)]);
+  firstNameFormControlErrorMessage = "";
+
   ngOnInit() {
     this.myForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl()
+      firstName: this.firstNameFormControl,
+      lastName: new FormControl("", Validators.required)
+    });
+
+    this.firstNameFormControl.valueChanges.subscribe( (value: string) => {
+
+      if ((this.firstNameFormControl.touched || this.firstNameFormControl.dirty) && this.firstNameFormControl.errors) {
+        this.firstNameFormControlErrorMessage = "error";
+      }
     });
   }
 
