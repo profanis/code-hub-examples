fd60225d32701ddba5db2948cfa26181c27a03ef
profanis <fanis@projectbeagle.com>
2018-09-30 16:04:10 +0300

    ligecycle changes

--- src/app/modules/lifecycle-example/life-cycle/life-cycle.component.ts
+++ src/app/modules/lifecycle-example/life-cycle/life-cycle.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit, AfterContentInit, OnChanges, SimpleChanges, DoCheck } from "@angular/core";
+import { Component, OnInit, AfterContentInit, OnChanges, SimpleChanges, DoCheck, AfterViewInit, ContentChild } from "@angular/core";
 
 @Component({
   selector: "app-life-cycle",
   templateUrl: "./life-cycle.component.html",
   styleUrls: ["./life-cycle.component.css"]
 })
-export class LifeCycleComponent implements OnInit, AfterContentInit, DoCheck {
+export class LifeCycleComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterViewInit {
 
   firstName: string;
 
   constructor() { }
 
   ngOnInit() {
-    console.log("On init");
+    console.log("Parent On init");
   }
 
   ngAfterContentInit(): void {
-    console.log("After content init");
+    console.log("Parent After content init");
   }
 
+  ngAfterViewInit(): void {
+    console.log("Parent After view init.");
+  }
+
+  /**
+   * It runs two times because we have ngModel attached to it
+   */
   ngDoCheck(): void {
-    console.log("Do Check");
+    console.log("Parent Do Check");
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("parentOnChnages");
   }
 
 }
