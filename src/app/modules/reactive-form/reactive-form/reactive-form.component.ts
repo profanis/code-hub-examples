08e93ca5d55abf1fe71a333887db3030cfdd4db6
Fanis Prodromou <fpr@anixe.io>
2019-01-21 16:00:29 +0200

    reactive forms example

--- src/app/modules/reactive-form/reactive-form/reactive-form.component.ts
+++ src/app/modules/reactive-form/reactive-form/reactive-form.component.ts
@@ -1,6 +1,8 @@
-import { ActivatedRoute, Router } from '@angular/router';
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators, AbstractControl } from "@angular/forms";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { Subscription } from "rxjs";
+
 import { Customer } from "../../basic-form/basic-form/cusomer.model";
 
 @Component({
@@ -8,17 +10,19 @@ import { Customer } from "../../basic-form/basic-form/cusomer.model";
   templateUrl: "./reactive-form.component.html",
   styleUrls: ["./reactive-form.component.css"]
 })
-export class ReactiveFormComponent implements OnInit {
+export class ReactiveFormComponent implements OnInit, OnDestroy {
 
   myForm: FormGroup;
   customer = new Customer();
   programmingLanguages = ["TS", "JS", "C#"];
+  firstNameFormControlSubscription: Subscription;
+  favouriteLanguageSubscription: Subscription;
 
   constructor(private router: Router) {
     // this.router.navigate(["/basic-form"], { queryParams: { key: "value"} });
    }
 
-  firstNameFormControl = new FormControl();
+  firstNameFormControl = new FormControl("", [Validators.required, Validators.minLength(3)]);
   firstNameFormControlErrorMessage = "";
   firstNameFormControlValidationMessages = {
     required : "The first name is required",
@@ -29,14 +33,15 @@ export class ReactiveFormComponent implements OnInit {
 
     this.myForm = new FormGroup({
       firstName: this.firstNameFormControl,
-      lastName: new FormControl(),
-      isExperienced: new FormControl(),
-      angular: new FormControl(),
-      favouriteLanguage: new FormControl()
+      lastName: new FormControl("", Validators.required),
+      isExperienced: new FormControl("", Validators.required),
+      angular: new FormControl("", Validators.required),
+      favouriteLanguage: new FormControl("", Validators.required),
+      jsversion: new FormControl()
     });
 
-    this.firstNameFormControl.valueChanges.subscribe( (value: string) => {
-      console.log(value);
+    this.firstNameFormControlSubscription = this.firstNameFormControl.valueChanges.subscribe( (value: string) => {
+      // console.log(value);
 
       this.firstNameFormControlErrorMessage = "";
 
@@ -46,11 +51,34 @@ export class ReactiveFormComponent implements OnInit {
         .map(c => this.firstNameFormControlValidationMessages[c]).join(" ");
       }
     });
+
+    this.favouriteLanguageSubscription = this.myForm.get("favouriteLanguage").valueChanges.subscribe(value => {
+      const jsVersionFormControl = this.myForm.get("jsversion");
+
+      if (value === "JS") {
+        jsVersionFormControl.setValidators(Validators.required);
+      } else {
+        jsVersionFormControl.clearValidators();
+      }
+      jsVersionFormControl.updateValueAndValidity();
+    });
+
+
   }
 
 
   submit({value}: {value}) {
     console.log(value);
+  }
+
+  ngOnDestroy(): void {
+    if (this.favouriteLanguageSubscription) {
+      this.favouriteLanguageSubscription.unsubscribe();
+    }
+
+    if (this.firstNameFormControlSubscription) {
+      this.firstNameFormControlSubscription.unsubscribe();
+    }
   }
 
 }
